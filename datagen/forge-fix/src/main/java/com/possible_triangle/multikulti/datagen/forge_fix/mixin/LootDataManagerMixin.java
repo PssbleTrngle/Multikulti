aec92f8974bda1e2c16a0f5b23d5f89dc7e56983
Niklas Widmann <nik@somethingcatchy.net>
2025-07-28 10:55:06 +0200

    catch invalid loot table json

--- datagen/forge-fix/src/main/java/com/possible_triangle/multikulti/datagen/forge_fix/mixin/LootDataManagerMixin.java
+++ datagen/forge-fix/src/main/java/com/possible_triangle/multikulti/datagen/forge_fix/mixin/LootDataManagerMixin.java
@@ -3,7 +3,6 @@ package com.possible_triangle.multikulti.datagen.forge_fix.mixin;
 import com.google.gson.JsonElement;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import java.util.Optional;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.world.level.storage.loot.LootDataManager;
@@ -15,21 +14,33 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
+import java.util.Optional;
+
 @Mixin(value = LootDataManager.class, remap = false)
 public class LootDataManagerMixin {
 
     @Unique
     private static final ICondition.IContext multikulti$context = ICondition.IContext.TAGS_INVALID;
 
+    @Unique
+    private static boolean multikulti$check(JsonElement json) {
+        if (!json.isJsonObject()) return true;
+        try {
+            return CraftingHelper.processConditions(json.getAsJsonObject(), "conditions", multikulti$context);
+        } catch (Exception ignored) {
+            return true;
+        }
+    }
+
     @WrapOperation(
             method = "lambda$scheduleElementParse$4(Lnet/minecraft/world/level/storage/loot/LootDataType;Lnet/minecraft/server/packs/resources/ResourceManager;Ljava/util/Map;Lnet/minecraft/resources/ResourceLocation;Lcom/google/gson/JsonElement;)V",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/storage/loot/LootDataType;deserialize(Lnet/minecraft/resources/ResourceLocation;Lcom/google/gson/JsonElement;Lnet/minecraft/server/packs/resources/ResourceManager;)Ljava/util/Optional;")
     )
     private static Optional<LootTable> respectConditions(LootDataType<?> instance, ResourceLocation id, JsonElement json, ResourceManager manager, Operation<Optional<LootTable>> original) {
-        if(!CraftingHelper.processConditions(json.getAsJsonObject(), "conditions", multikulti$context)) {
-            return Optional.empty();
-        } else {
+        if (instance != LootDataType.TABLE || multikulti$check(json)) {
             return original.call(instance, id, json, manager);
+        } else {
+            return Optional.empty();
         }
     }
 
